0281321f018116bf45e5a78c77f88f932db29975
Narendras <n.syahrasyad.n@gmail.com>
2018-12-10 16:37:03 -0800

    Updated with first problem

---
+++ problems/easy/jumping-on-clouds.js
@@ -0,0 +1,14 @@
+'use strict'
+
+function jumpingOnClouds(clouds) {
+    let pos = 0;
+    let jumps = 0;
+    while (pos < clouds.length - 1) {
+        const toJump = pos + 2;
+        pos = clouds[toJump] ? pos + 1 : toJump;
+        jumps++;
+    }
+    return jumps;
+}
+
+module.exports = jumpingOnClouds;
---
+++ problems/easy/jumping-on-clouds.test.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const jumpingOnClouds = require('./jumping-on-clouds');
+
+const c1 = [0,0,1,0,0,1,0];
+const c2 = [0,0,0,0,1,0];
+
+test("Minimum jumps for [0,0,1,0,0,1,0] is 4", () => {
+    expect(jumpingOnClouds(c1)).toEqual(4);
+});
+
+test("Minimum jumps for [0,0,0,0,1,0] is 3", () => {
+    expect(jumpingOnClouds(c2)).toEqual(3);
+});
