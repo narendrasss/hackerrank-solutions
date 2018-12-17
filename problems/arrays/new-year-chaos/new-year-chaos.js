d63eb25731a96b784e7b31ae9dcb697956de0b3f
Narendras <n.syahrasyad.n@gmail.com>
2018-12-17 02:47:52 -0800

    Solved new years challenge

--- problems/arrays/new-year-chaos/new-year-chaos.js
+++ problems/arrays/new-year-chaos/new-year-chaos.js
@@ -8,14 +8,50 @@
  * Given the current state of the queue, determine the minimum 
  * number of bribes required to get the array to its current place.
  * 
+ * The initial state is always [1, 2, 3, ..., n].
+ * 
  * If the line configuration is not possible, print 'too chaotic'.
+ * 
+ * TODO: Optimize this! Current runtime -> O(n^2)
  * 
  * @param {Number[]} q the current queue state
  * @returns {Number | String} the minimum number of bribes or
  *                            'too chaotic' if not possible.
  */
 function minimumBribes(q) {
+    if (!checkPossible(q)) return 'Too chaotic';
 
+    let numBribes = 0;
+    for (let i = 0; i < q.length - 1; i++) {
+        for (let k = 0; k < q.length - i - 1; k++) {
+            if (q[k] > q[k + 1]) {
+                swap(q, k, k + 1);
+                numBribes++;
+            }
+        }
+    }
+    return numBribes;
+}
+
+/**
+ * Checks if the given queue is a valid configuration. 
+ * @param {Number[]} q the queue to check
+ * @returns {Boolean} true if q is valid, false otherwise.
+ */
+function checkPossible(q) {
+    return q.filter((el, i) => el - i > 3).length == 0;
+}
+
+/**
+ * Swaps the elements at indices i and j.
+ * @param {Number[]} q 
+ * @param {Number} i 
+ * @param {Number} j 
+ */
+function swap(q, i, j) {
+    const temp = q[i];
+    q[i] = q[j];
+    q[j] = temp;
 }
 
 module.exports = minimumBribes;
--- problems/arrays/new-year-chaos/new-year-chaos.test.js
+++ problems/arrays/new-year-chaos/new-year-chaos.test.js
@@ -4,6 +4,7 @@ const minimumBribes = require('./new-year-chaos');
 
 const ex1 = [2,1,5,3,4];
 const ex2 = [2,5,1,3,4];
+const ex3 = [1,2,5,3,4,7,8,6];
 
 test('Example 1 has 3 minimum bribes', () => {
     expect(minimumBribes(ex1)).toBe(3);
@@ -11,4 +12,8 @@ test('Example 1 has 3 minimum bribes', () => {
 
 test('Example 2 is too chaotic', () => {
     expect(minimumBribes(ex2)).toEqual('Too chaotic');
+});
+
+test('Example 3 has 4 minimum bribes', () => {
+    expect(minimumBribes(ex3)).toBe(4);
 });
