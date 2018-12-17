27cf02e08b15c2c76a1d7b82e9767ae3fea4247b
Narendras <n.syahrasyad.n@gmail.com>
2018-12-16 19:58:15 -0800

    Solved 2d-array problem

---
+++ problems/arrays/2d-array/2d-array.js
@@ -0,0 +1,43 @@
+'use strict'
+
+/**
+ * Brute force approach.
+ * @param {Number[][]} arr a 6x6 2d array
+ * @returns {Number}
+ */
+function hourglassSum(arr) {
+    const hourglasses = getAllHourglasses(arr);
+    const sums = hourglasses.map(getSum);
+    return Math.max(...sums);
+}
+
+/**
+ * Returns an array of all the hourglasses in arr.
+ * @param {*} arr 
+ * @returns {Number[][]}
+ */
+function getAllHourglasses(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length - 2; i++) {
+        for (let k = 0; k < arr[i].length - 2; k++) {
+            let hourglass = [
+                arr[k][i], arr[k][i + 1], arr[k][i + 2],
+                arr[k + 1][i + 1],
+                arr[k + 2][i], arr[k + 2][i + 1], arr[k + 2][i + 2]
+            ];
+            result.push(hourglass);
+        }
+    }
+    return result;
+}
+
+/**
+ * Returns the sum of this hourglass.
+ * @param {Number[]} hourglass 
+ * @returns {Number}
+ */
+function getSum(hourglass) {
+    return hourglass.reduce((acc, num) => acc += num);
+}
+
+module.exports = hourglassSum;
---
+++ problems/arrays/2d-array/2d-array.test.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const hourglassSum = require('./2d-array');
+
+const c1 = [
+    [1,1,1,0,0,0],
+    [0,1,0,0,0,0],
+    [1,1,1,0,0,0],
+    [0,0,2,4,4,0],
+    [0,0,0,2,0,0],
+    [0,0,1,2,4,0]
+]
+
+test('Array c1 has maximum 19', () => {
+    expect(hourglassSum(c1)).toEqual(19);
+});
